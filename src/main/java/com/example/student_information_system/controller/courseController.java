4e6b5d51ba902c928341f88efbe25b894f55f517
ahmet okur <41aokur41@gmail.com>
2022-08-10 22:47:52 +0300

    course controller added

---
+++ src/main/java/com/example/student_information_system/controller/courseController.java
@@ -0,0 +1,38 @@
+package com.example.student_information_system.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.student_information_system.domain.course;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+@RequiredArgsConstructor
+public class courseController {
+
+  private final courseService courseService;
+
+  @GetMapping
+  public List<course> getCourses() {
+    return null;
+  }
+
+  @PostMapping
+  public void registerNewCourse(@RequestBody course course) {
+    courseService.addNewStudent(course);
+  }
+
+  @DeleteMapping("/{course}")
+  public void deleteCourse(@PathVariable("courseId") Long courseId) {
+    courseService.deleteCourse(courseId);
+  }
+}
