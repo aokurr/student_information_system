d5f584ae827ec7619ca5aed2055e6489d8ce30a3
ahmet okur <41aokur41@gmail.com>
2022-08-10 15:12:54 +0300

    student controller added

---
+++ src/main/java/com/example/student_information_system/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.example.student_information_system.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.student_information_system.domain.student;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+@RequiredArgsConstructor
+public class studentController {
+	
+	private final studentService studentService;
+		
+	@GetMapping
+	public List<student> getStudents(){
+		return null;
+    }
+   
+    @PostMapping
+    public void registerNewStudent(@RequestBody student student){
+		studentService.addNewStudent(student);
+    }
+    @DeleteMapping("/{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+		studentService.deleteStudent(studentId);
+    }
+}
