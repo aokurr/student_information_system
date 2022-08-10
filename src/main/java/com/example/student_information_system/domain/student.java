8603c625441917446811ee35df549413933c6623
ahmet okur <41aokur41@gmail.com>
2022-08-10 14:59:44 +0300

    student entity added

---
+++ src/main/java/com/example/student_information_system/domain/student.java
@@ -0,0 +1,29 @@
+package com.example.student_information_system.domain;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+public class student {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false, updatable = false)
+    private long id;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "surname")
+    private String surname;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "password")
+    private String password;
+        
+}
