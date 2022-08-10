6af37c2e7933b6ac3a0b8368226218a73cc916e6
ahmet okur <41aokur41@gmail.com>
2022-08-10 14:51:40 +0300

    first upload

---
+++ src/test/java/com/example/student_information_system/StudentInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_information_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
